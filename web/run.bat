3c8947ed203e7ef004487da4bb668e6d0fa6547a
Victor Rodriguez Doncel <victorr@ac.upc.edu>
2018-12-19 18:48:48 +0100

    deleteing old version, to make a new refactor

--- web/run.bat
+++
@@ -1,6 +0,0 @@
-REM This application requires the following steps:
-REM - Start Tomcat server (run "startup" in the tomcat). In salonica: C:\Java\apache-tomcat-8.0.23\bin
-REM - Deploy the war application. For example via http://localhost:8080/manager/html
-REM - Start a fuseki linked data server in port 3030. For example, In salonica, C:\Java\apache-jena-fuseki-2.0.0. Run "fuseki-server --loc=data /RDFChess"
-REM - 
-REM
