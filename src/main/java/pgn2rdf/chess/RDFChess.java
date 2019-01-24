f3f32be7c448e6126de38ae44731aa0730e56212
Víctor Rodríguez Doncel <victorr@ac.upc.edu>
2019-01-24 18:38:29 +0100

    coge fuseki como webapp

--- src/main/java/pgn2rdf/chess/RDFChess.java
+++ src/main/java/pgn2rdf/chess/RDFChess.java
@@ -11,7 +11,9 @@ public class RDFChess {
     public static final String ONTOLOGY_URI = "http://purl.org/NET/rdfchess/ontology/";
     
 //    public static final String sparqlu = "http://localhost:3030/RDFChess/update";
-    public static final String sparqlu = "http://localhost:3030/RDFChess/update";
-    public static final String sparqlq = "http://localhost:3030/RDFChess/query";
+//    public static final String sparqlq = "http://localhost:3030/RDFChess/query";
+
+    public static final String sparqlu = "http://localhost:8082/fuseki/RDFChess/update";
+    public static final String sparqlq = "http://localhost:8082/fuseki/RDFChess/query";
 
 }
